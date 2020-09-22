fef586819dff4952cbc1c4c029c18b84f5aeba06
mani9896 <shankar.money08@gmail.com>
2020-09-22 21:23:49 +0530

    Made some changes in log in and added log out

--- app.js
+++ app.js
@@ -3,177 +3,171 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
-const fs = require('fs'); 
-const path = require('path'); 
-const multer = require('multer'); 
-require('dotenv/config');
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+const multer = require("multer");
+require("dotenv/config");
+const homeStartingContent =
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const app = express();
-var isLogged=false;
-app.set('view engine', 'ejs');
-var userName="";
-app.use(bodyParser.urlencoded({extended: true}));
+var isLogged = false;
+app.set("view engine", "ejs");
+var userName = "";
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/blogDB", { useNewUrlParser: true });
 
-
-var storage = multer.diskStorage({ 
-  destination: (req, file, cb) => { 
-      cb(null, 'uploads') 
-      console.log("kjbjhbjh");
-  }, 
-  filename: (req, file, cb) => { 
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
     console.log("kjbjhbjh");
-      cb(null, file.fieldname + '-' + Date.now()) 
-  } 
-}); 
+  },
+  filename: (req, file, cb) => {
+    console.log("kjbjhbjh");
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
 
-var upload = multer({ storage: storage }); 
-
+var upload = multer({ storage: storage });
 
 const postSchema = {
-  name:String,
+  name: String,
   title: String,
   content: String,
-  img: 
-    { 
-        data: Buffer, 
-        contentType: String 
-    }
+  img: {
+    data: Buffer,
+    contentType: String,
+  },
 };
-
 
 const Post = mongoose.model("Post", postSchema);
 const userSchema = {
   name: {
-    type:String,
-    required: true
+    type: String,
+    required: true,
   },
   email: {
-  type:String,
-    required:true
+    type: String,
+    required: true,
   },
   password: {
-    type:String,
-    required:true
-  }
+    type: String,
+    required: true,
+  },
 };
 
 const User = mongoose.model("User", userSchema);
 
-app.get("/", function(req, res){
-
-  Post.find({}, function(err, posts){
+app.get("/", function (req, res) {
+  Post.find({}, function (err, posts) {
     res.render("home", {
       startingContent: homeStartingContent,
-      posts: posts
-      });
-  });
-});
-
-app.get("/compose", function(req, res){
-  if(isLogged)
-  {
-  res.render("compose");
-  }
-  else{
-    res.render("LogIn");
-  }
-});
-app.post('/compose', upload.single('image'), (req, res, next) => { 
-  
-  var obj = { 
-      name: req.body.name, 
-      title: req.body.postTitle,
-      content: req.body.postBody,
-      img: { 
-          data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)), 
-          contentType: 'image/png'
-      } 
-  } 
-  Post.create(obj, (err, item) => { 
-      if (err) { 
-          console.log(err); 
-      } 
-      else { 
-          // item.save(); 
-          res.redirect('/'); 
-      } 
-  }); 
-}); 
-
-app.get("/posts/:postId", function(req, res){
-
-const requestedPostId = req.params.postId;
-
-  Post.findOne({_id: requestedPostId}, function(err, post){
-    res.render("post", {
-      name:post.name,
-      title: post.title,
-      content: post.content,
-      image:post.img
+      posts: posts,
+      isLogged: isLogged,
     });
   });
-
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+app.get("/compose", function (req, res) {
+  if (isLogged) {
+    res.render("compose", { isLogged: isLogged });
+  } else {
+    res.render("LogIn", { isLogged: isLogged });
+  }
 });
-
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
-});
-app.get("/LogIn",function(req,res){
-  res.render("LogIn");
-});
-app.get("/sign-up",function(req,res){
-  res.render("sign-up");
-});
-app.post("/sign-up",function(req,res){
-  const new_user=new User({
-      name:req.body.name,
-      email:req.body.email,
-      password:req.body.password
+app.post("/compose", upload.single("image"), (req, res, next) => {
+  var obj = {
+    name: req.body.name,
+    title: req.body.postTitle,
+    content: req.body.postBody,
+    img: {
+      data: fs.readFileSync(
+        path.join(__dirname + "/uploads/" + req.file.filename)
+      ),
+      contentType: "image/png",
+    },
+  };
+  Post.create(obj, (err, item) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // item.save();
+      res.redirect("/");
+    }
   });
-  
-  new_user.save(function(err){
+});
+
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    res.render("post", {
+      name: post.name,
+      title: post.title,
+      content: post.content,
+      image: post.img,
+      isLogged: isLogged,
+    });
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent, isLogged: isLogged });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent, isLogged: isLogged });
+});
+app.get("/LogIn", function (req, res) {
+  res.render("LogIn", { isLogged: isLogged });
+});
+app.get("/LogOut", function (req, res) {
+  isLogged = false;
+  res.redirect("/");
+});
+app.get("/sign-up", function (req, res) {
+  res.render("sign-up", { isLogged: isLogged });
+});
+app.post("/sign-up", function (req, res) {
+  const new_user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  new_user.save(function (err) {
     console.log("bjbhkbhj");
-    if (!err){
-        res.redirect("/LogIn");
-    }
-    else{
+    if (!err) {
+      res.redirect("/LogIn");
+    } else {
       console.log(err);
     }
-    
   });
 });
-app.post("/LogIn",function(req,res){
-  const email=req.body.email;
-  const password=req.body.password;
+app.post("/LogIn", function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
   console.log(email);
-  User.findOne({ email: email }, function(err, user) {
-    if (err){
+  User.findOne({ email: email }, function (err, user) {
+    if (err) {
       console.log(err);
-    }
-    else{
-      if(user==null)
-      {
+    } else {
+      if (user == null) {
         console.log("not found");
-      }
-      else{
-        if(user.password==password){
-          isLogged=true;
-          userName=user.name;
+      } else {
+        if (user.password == password) {
+          isLogged = true;
+          userName = user.name;
           console.log(userName);
           res.redirect("/compose");
-        }
-        else{
-          res.redirect("/LogIn")
+        } else {
+          res.redirect("/LogIn");
         }
       }
     }
@@ -182,11 +176,11 @@ app.post("/LogIn",function(req,res){
     // user.comparePassword(password, function(err, isMatch) {
     //     if (err) throw err;
     //     console.log(password, isMatch); // -> Password123: true
-    });
+  });
 
-    // test a failing password
+  // test a failing password
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
